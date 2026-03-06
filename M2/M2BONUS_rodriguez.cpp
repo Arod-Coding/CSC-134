0524c63b3a89fd14c1b7d435e4682d3f2b4e652f
Arod-Coding <rodrigua9906@student.faytechcc.edu>
2026-03-06 09:06:33 +0000

    Did Prob 1 & 2 for M2 Bonus

---
+++ M2/M2BONUS_rodriguez.cpp
@@ -0,0 +1,29 @@
+// M2BONUS
+// Adrian Rodriguez
+// 3/6/26
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Prob 1: Rectangle Area
+    cout << "Problem 1: Rectangle Area" << endl;
+    int height = 8;
+    int width = 10;
+    int area = height * width;
+    cout << "Height = " << height << ", width = " << width << endl;
+    cout << "Area of rectangle = " << area << endl << endl;
+
+    // Prob 2: Product price with sales tax
+    cout << "Problem 2: Product Price with Sales Tax" << endl;
+    double price = 99.99;
+    double taxRate = 0.08;
+    double taxAmount = price * taxRate;
+    double totalCost = price + taxAmount;
+    cout << "Product price = $" << price << endl;
+    cout << "Sales tax (8%) = $" << taxAmount << endl;
+    cout << "Total cost = $" << totalCost << endl << endl;
+
+    // Prob 3: Pizza area
+}
