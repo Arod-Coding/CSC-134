debb35c670a54a29b1f00476215c91be77a38258
Arod-Coding <rodrigua9906@student.faytechcc.edu>
2026-04-28 21:19:12 +0000

    finished M5LAB2

---
+++ M5/M5LAB2_rodriguez.cpp
@@ -0,0 +1,63 @@
+// M5LAB2
+//Adrian Rodriguez
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Function prototypes
+double getLength();
+double getWidth();
+double getArea(double length, double width);
+void displayData (double length, double width, double area);
+
+int main()
+{
+    // Calculates the area of a rectangle.
+
+    double length,
+            width,
+            area;
+
+    length = getLength();
+    width = getWidth();
+    area = getArea(length, width);
+
+    return 0;
+
+}
+
+// Get Length
+
+double getLength() {
+    double length;
+    cout << "Enter the length of the rectangle: ";
+    cin >> length;
+    return length;
+}
+
+// get width
+
+double getWidth() {
+    double width;
+    cout << "Enter the width of the rectangle: ";
+    cin >> width;
+    return width;
+}
+
+// get area
+
+double getArea(double length, double width) {
+    return length * width;
+}
+
+// display data
+
+void displayData(double length, double width, double area) {
+    cout << fixed << setprecision(2);
+    cout << "\nRectangle Data\n";
+    cout << "----------------------\n";
+    cout << "Length: " << length << endl;
+    cout << "Width : " << width << endl;
+    cout << "Area  : " << area << endl;
+}
