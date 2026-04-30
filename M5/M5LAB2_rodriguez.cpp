9377c7754d2171123352ca228344d4205a87d104
Arod-Coding <rodrigua9906@student.faytechcc.edu>
2026-04-30 09:17:25 +0000

    finished M5LAB2

--- M5/M5LAB2_rodriguez.cpp
+++ M5/M5LAB2_rodriguez.cpp
@@ -15,49 +15,49 @@ int main()
 {
     // Calculates the area of a rectangle.
 
-    double length,
-            width,
-            area;
+        double length,
+                    width,
+                                area;
 
-    length = getLength();
-    width = getWidth();
-    area = getArea(length, width);
+                                    length = getLength();
+                                        width = getWidth();
+                                            area = getArea(length, width);
+                                            displayData(length, width, area);
+                                                return 0;
 
-    return 0;
+                                                }
 
-}
+                                                // Get Length
 
-// Get Length
+                                                double getLength() {
+                                                    double length;
+                                                        cout << "Enter the length of the rectangle: ";
+                                                            cin >> length;
+                                                                return length;
+                                                                }
 
-double getLength() {
-    double length;
-    cout << "Enter the length of the rectangle: ";
-    cin >> length;
-    return length;
-}
+                                                                // get width
 
-// get width
+                                                                double getWidth() {
+                                                                    double width;
+                                                                        cout << "Enter the width of the rectangle: ";
+                                                                            cin >> width;
+                                                                                return width;
+                                                                                }
 
-double getWidth() {
-    double width;
-    cout << "Enter the width of the rectangle: ";
-    cin >> width;
-    return width;
-}
+                                                                                // get area
 
-// get area
+                                                                                double getArea(double length, double width) {
+                                                                                    return length * width;
+                                                                                    }
 
-double getArea(double length, double width) {
-    return length * width;
-}
+                                                                                    // display data
 
-// display data
-
-void displayData(double length, double width, double area) {
-    cout << fixed << setprecision(2);
-    cout << "\nRectangle Data\n";
-    cout << "----------------------\n";
-    cout << "Length: " << length << endl;
-    cout << "Width : " << width << endl;
-    cout << "Area  : " << area << endl;
-}
+                                                                                    void displayData(double length, double width, double area) {
+                                                                                        cout << fixed << setprecision(2);
+                                                                                            cout << "\nRectangle Data\n";
+                                                                                                cout << "----------------------\n";
+                                                                                                    cout << "Length: " << length << endl;
+                                                                                                        cout << "Width : " << width << endl;
+                                                                                                            cout << "Area  : " << area << endl;
+                                                                                                            }
